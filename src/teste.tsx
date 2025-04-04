229ad400f1c16a1609c639901839bc9aae12d021
ariel <you@example.com>
2025-04-04 18:10:30 -0300

    inciando as configurações

---
+++ src/teste.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+  fontSize?: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontSize || 'px'};
+`
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+
+  span {
+    text-decoration: line-through;
+  }
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao principal>Enviar</Botao>
+      <Botao fontSize="14px" principal={false}>
+        Cancelar
+      </Botao>
+      <BotaoPerigo principal as="a">
+        <span> Não clique</span>
+      </BotaoPerigo>
+    </>
+  )
+}
+
+export default Teste
