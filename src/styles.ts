87d788f5c883dc9d197ad8702d0ed9926a2c11bb
ariel <you@example.com>
2025-04-07 18:18:37 -0300

    criando a seção de projetos e o componente de projeto

--- src/styles.ts
+++ src/styles.ts
@@ -7,6 +7,7 @@ const EstiloGlobal = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: "Inter", sans-serif;
+    list-style: none;
   }
 
   body {
@@ -37,6 +38,5 @@ export const Container = styled.div`
 
   img {
     max-width: 100%;
-    height: auto;
   }
 `
